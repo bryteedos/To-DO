0df8d6e203b2637c48aecc7e650eaa4c49b1b64d
Bright Edokpayi <bright.edokpayi@yahoo.com>
2022-02-08 10:56:44 +0100

    modified: taskmate/settings.py

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import environ
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,6 +131,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL='todolist'
 LOGIN_URL='login'
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
